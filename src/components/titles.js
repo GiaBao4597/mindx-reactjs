0bd6a68c0deacb93a9ef1d4f75ace75a35fcc5e8
Gia Bảo <giabao@MacBook-Pro-cua-Gia.local>
2023-04-06 19:20:39 +0700

    reactjs

---
+++ src/components/titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+                <h1 className="title-container__title">Weather Scanner </h1>
+            </div>
+        )
+    }
+}
+
+export default Titles;
